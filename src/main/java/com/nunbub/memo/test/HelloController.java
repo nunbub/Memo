a9fe617bf588911ccd6a0d4c5bff26e78f10b2f6
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-30 14:27:37 +0900

    [류지수] - 최초 파일 추가

---
+++ src/main/java/com/nunbub/memo/test/HelloController.java
@@ -0,0 +1,17 @@
+package com.nunbub.memo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "hello world!!";
+		
+	}
+	
+}
