3fa25ad8315efa5a739ffea3e2c9d2f023e8f589
DESKTOP-1E01CNG\Ryu <starjisu519@naver.com>
2022-08-31 14:18:49 +0900

    [류지수] - yml 을 통한 jsp database 성정 - jsp 관련 build gradle 수정 - nunbub/Memo#1 이슈 정리

--- src/main/java/com/nunbub/memo/MemoApplication.java
+++ src/main/java/com/nunbub/memo/MemoApplication.java
@@ -1,12 +1,12 @@
 package com.nunbub.memo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 public class MemoApplication {
 
 	public static void main(String[] args) {
